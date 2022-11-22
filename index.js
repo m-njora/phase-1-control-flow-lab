63492c29004101a071edee7134240cdbaeccb5b4
m-njora <mmuthekijora@gmail.com>
2022-11-22 23:29:32 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  // Write your code here!}
+  if (someValue <= 400){
+    return "This one is on me!";
+}
+else if (someValue > 2000 && someValue < 2500){
+return "I will gladly take your thirty bucks.";
+}
+else if(someValue > 2500){
+return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+}
+else{
+    return "No go.";
+}
+}
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+} else if (tip === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
+}
 }
